73d7f576a9adaf8e387fae0efd2a5b51491ed3b1
Instant-Monkey <sylvain.laugier.net@gmail.com>
2015-12-09 02:30:29 +0100

    Story done til france-2

--- js/make-it-move.js
+++ js/make-it-move.js
@@ -1,5 +1,6 @@
 $( document ).ready(function() {
 	"use strict";
+	
 
 /* ---------------------------------- *\ 
   #SKILLS SECTION 
@@ -112,6 +113,9 @@ $( document ).ready(function() {
 	$(".realization__arrow-left").click(slideRealLeft); 
 	$(".realization__arrow-right").click(slideRealRight); 
 	
+	
+	
+	
 /* ---------------------------------- *\ 
   #STORY SECTION 
 \* ---------------------------------- */
@@ -119,109 +123,477 @@ $( document ).ready(function() {
 	// init controller
 var controller = new ScrollMagic.Controller();
 
+
+
+
+
 //-------------TITLE MY STORY -------------//
 
 // Keep the title my story centered during 100 vh 
 new ScrollMagic.Scene({
-		triggerElement: '#story .story-intro',
+		triggerElement: '.story__intro',
         duration: (100*vh()),    // last 100 vh 
         offset: (50*vh())       // start this scene after scrolling for 50 vh 
     })
-    .setPin("#story .story-intro .story-intro-title") // pins the element for the the scene's duration
+    .setPin(".story__intro-title") // pins the element for the the scene's duration
     .addIndicators()
 	.addTo(controller); // assign the scene to the controller
+	
+	
+	
+	
+	
+	
 
 //-------------FIX LIST YEAR -------------//
 
-// Keep the list year fixed during the whole scroll  
+// Keep the list year fixed during the whole scroll 
+ 
 new ScrollMagic.Scene({
-		triggerElement: '#story .story-wrapper',
-        duration: 400*vh(),    // last 100 vh 
+		triggerElement: '.story__wrapper',
+        duration: 1200*vh(),    // last 100 vh 
         offset: 50*vh()       // start this scene after scrolling for 50 vh 
     })
-    .setPin("#story .story-wrapper .story-list-year ul") // pins the element for the the scene's duration
+    .setPin(".story__list-year ul") // pins the element for the the scene's duration
     .addIndicators()
-	.addTo(controller); // assign the scene to the controller
+	.addTo(controller); // assign the scene to the controller  
+	
+//  Make the year font white for blue background 
 
-//-------------DISPLAY/HIDE DESCRIPTION IUT -------------//
+new ScrollMagic.Scene({
+		triggerElement: '.story__canada',
+        duration: 225*vh(),    
+        offset: 25*vh()       // start this scene after scrolling for 50 vh 
+    })
+    .setClassToggle(".story__year", "story__year--white")
+    .addIndicators()
+	.addTo(controller); // assign the scene to the controller  
 	
-var displayDescriptionIut = new ScrollMagic.Scene({
-	triggerElement: ".story-france-1",
-	duration:150*vh(),
-	offset:100*vh()
-	})					
-	
-	.setTween(".wrapper-description-iut", 0.2, {opacity: "6" })	
-	.setClassToggle(".wrapper-description-iut", "fixed-wrapper-iut")
-	.addIndicators()				
-	.addTo(controller);
-
-
-	
-var hideDescriptionIut = new ScrollMagic.Scene({
-	triggerElement: ".story-france-1",
-	duration:50*vh(),
-	offset:200*vh()
-	})						
-	
-	.setTween(".wrapper-description-iut", 0.2, {opacity: "0" })	
-	.addIndicators()				
-	.addTo(controller); 
-	
-//-------------LINE TO CANADA-------------//
-	
-	
-	// prepare SVG
-	var $path = $("#line-to-canada");
-	pathPrepare($path);
-	
-	var tween = new TimelineMax()
-		.add(TweenMax.to($path, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
-		//.add(TweenMax.to("path", 1, {stroke: "#fff", ease:Linear.easeNone}), 0);			// change color during the whole thing
-		
-	var scene = new ScrollMagic.Scene({triggerElement: ".story-france-1", duration: 100*vh(), offset:200*vh(), tweenChanges: true})
-					.setTween(tween)
-					.setClassToggle(".wrapper-line-to-canada","fixed-wrapper-line-to-canada")
-					.addIndicators() // add indicators (requires plugin)
-					.addTo(controller);
 
 //-------------2009 -------------//
 
 var toggleYear2009 = new ScrollMagic.Scene({
-	triggerElement: "#story .story-wrapper",
+	triggerElement: ".story__wrapper",
 	duration:100*vh(),
 	offset: 50*vh()
 	})						
 	
-	.setClassToggle(".y2009", "active")	
+	.setClassToggle(".story__y2009", "story__year--active")	
 	.addIndicators()				
 	.addTo(controller); 
 
 //-------------2010 -------------//
 	
 var toggleYear2010 = new ScrollMagic.Scene({
-	triggerElement: "#story .story-wrapper",
+	triggerElement: ".story__wrapper",
 	duration:50*vh(),
 	offset: (150*vh())
 	})						
 	
-	.setClassToggle(".y2010", "active")
+	.setClassToggle(".story__y2010", "story__year--active")
 	.addIndicators()					
 	.addTo(controller); 
 	
 //-------------2011 -------------//
 	
 var toggleYear2011 = new ScrollMagic.Scene({
-	triggerElement: "#story .story-wrapper",
-	duration:100*vh(),
+	triggerElement: ".story__wrapper",
+	duration:450*vh(),
 	offset: (200*vh())
 	})						
 	
-	.setClassToggle(".y2011", "active")
+	.setClassToggle(".story__y2011", "story__year--active")
+	.addIndicators()					
+	.addTo(controller); 
+
+//-------------2012 -------------//
+
+var toggleYear2011 = new ScrollMagic.Scene({
+	triggerElement: ".story__wrapper",
+	duration:250*vh(),
+	offset: (650*vh())
+	})						
+	
+	.setClassToggle(".story__y2012", "story__year--active")
+	.addIndicators()					
+	.addTo(controller); 
+
+//-------------2013 -------------//
+
+var toggleYear2011 = new ScrollMagic.Scene({
+	triggerElement: ".story__wrapper",
+	duration:200*vh(),
+	offset: (900*vh())
+	})						
+	
+	.setClassToggle(".story__y2013", "story__year--active")
 	.addIndicators()					
 	.addTo(controller); 
 	
+	
+	
+	
+	
+
+//-------------DISPLAY/HIDE DESCRIPTION IUT -------------//
+
+//Toggle the class which 'display:block' the div 
+
+var displayDescriptionIut = new ScrollMagic.Scene({
+		triggerElement: ".story__france-1",
+		duration:125*vh(),
+		offset:100*vh()
+	})	
+					
+	.setClassToggle(".story__wrapper-description-iut","story__wrapper-description-iut--display")
+	.addIndicators()				
+	.addTo(controller);
+	
+// Setting the Fade In 
+
+var fadeInDescriptionIut = new ScrollMagic.Scene({
+		triggerElement: ".story__france-1",
+		duration:25*vh(),
+		offset:100*vh()
+	})		
+				
+	.setTween(".story__wrapper-description-iut", 0.2, {opacity: "1" })	
+	.addIndicators()				
+	.addTo(controller);
+
+
+// Setting the Fade Out 
+	
+var fadeOutDescriptionIut = new ScrollMagic.Scene({
+	triggerElement: ".story__france-1",
+	duration:25*vh(),
+	offset:200*vh()
+	})						
+	
+	.setTween(".story__wrapper-description-iut", 0.2, {opacity: "0" })	
+	.addIndicators()				
+	.addTo(controller); 
+	
+	
+	
+	
+	
+	
+//-------------LINE TO CANADA-------------//
+	
+	
+// prepare SVG
+
+var $canadaPath = $("#story__line-to-canada");
+pathPrepare($canadaPath);
+
+
+// Setting of the tween 
+
+var drawFranceCanadaLine = new TimelineMax()
+	.add(TweenMax.to($canadaPath, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+	//.add(TweenMax.to("path", 1, {stroke: "#fff", ease:Linear.easeNone}), 0);			// change color during the whole thing
+
+
+// Display the div that contains the line 
+
+var displayLineToCanada = new ScrollMagic.Scene({
+	triggerElement: ".story__france-1",
+	duration: 100*vh(),
+	offset:200*vh(),
+	})
+	
+	.setClassToggle(".story__wrapper-line-to-canada","story__wrapper-line-to-canada--display")
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
+	
+	
+// Draw the line  
+		
+var drawLineToCanada = new ScrollMagic.Scene({
+	triggerElement: ".story__france-1",
+	duration: 50*vh(),
+	offset: 200*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween(drawFranceCanadaLine )
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
+	
+	
+// Erase the line 
+
+var eraseLineToCanada = new ScrollMagic.Scene({
+	triggerElement: ".story__france-1",
+	duration: 25*vh(),
+	offset: 250*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween("#story__line-to-canada", 0.2, {opacity: "0" })
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller); 
+	
+var upLineToCanada = new ScrollMagic.Scene({
+	triggerElement: ".story__france-1",
+	duration: 50*vh(),
+	offset: 250*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween(".story__wrapper-line-to-canada", 0.2, {top: "-400px"})
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller); 
+
+	
+	
+	
+	
+	
+	
+	
+//-------------DISPLAY/HIDE DESCRIPTION CANADA -------------//
+
+//Toggle the class which 'display:block' the div 
+
+var displayDescriptionIut = new ScrollMagic.Scene({
+		triggerElement: ".story__canada",
+		duration:125*vh(),
+		offset:50*vh()
+	})	
+					
+	.setClassToggle(".story__wrapper-description-canada","story__wrapper-description-canada--display")
+	.addIndicators()				
+	.addTo(controller);
+	
+// Setting the Fade In 
+
+var fadeInDescriptionIut = new ScrollMagic.Scene({
+		triggerElement: ".story__canada",
+		duration:25*vh(),
+		offset:50*vh()
+	})		
+				
+	.setTween(".story__wrapper-description-canada", 0.2, {opacity: "1" })	
+	.addIndicators()				
+	.addTo(controller);
+
+
+// Setting the Fade Out 
+	
+var fadeOutDescriptionIut = new ScrollMagic.Scene({
+	triggerElement: ".story__canada",
+	duration:25*vh(),
+	offset:150*vh()
+	})						
+	
+	.setTween(".story__wrapper-description-canada", 0.2, {opacity: "0" })	
+	.addIndicators()				
+	.addTo(controller); 
+
+
+
+
+
+
+//-------------LINE TO FINLAND-------------//
+	
+	
+// prepare SVG
+
+var $finlandPath = $("#story__line-to-finland");
+pathPrepare($finlandPath);
+
+
+// Setting of the tween 
+
+var drawCanadaFinlandLine = new TimelineMax()
+	.add(TweenMax.to($finlandPath, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+
+
+// Display the div that contains the line 
+
+var displayLineToFinland = new ScrollMagic.Scene({
+	triggerElement: ".story__canada",
+	duration: 100*vh(),
+	offset:150*vh(),
+	})
+	
+	.setClassToggle(".story__wrapper-line-to-finland","story__wrapper-line-to-finland--display")
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
+	
+	
+// Draw the line  
+		
+var drawLineToFinland = new ScrollMagic.Scene({
+	triggerElement: ".story__canada",
+	duration: 50*vh(),
+	offset: 150*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween(drawCanadaFinlandLine)
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
+	
+	
+// Erase the line 
+
+var eraseLineToFinland = new ScrollMagic.Scene({
+	triggerElement: ".story__canada",
+	duration: 50*vh(),
+	offset: 200*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween("#story__line-to-finland", 0.2, {opacity: "0" })
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller); 
+	
+var upLineToFinland = new ScrollMagic.Scene({
+	triggerElement: ".story__canada",
+	duration: 50*vh(),
+	offset: 200*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween(".story__wrapper-line-to-finland", 0.2, {top: "-200px"})
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller); 
+	
+	
+	
+	
+	
+	
+
+
+//-------------DISPLAY/HIDE DESCRIPTION FINLAND -------------//
+
+//Toggle the class which 'display:block' the div 
+
+var displayDescriptionIut = new ScrollMagic.Scene({
+		triggerElement: ".story__finland",
+		duration:125*vh(),
+		offset:50*vh()
+	})	
+					
+	.setClassToggle(".story__wrapper-description-finland","story__wrapper-description-finland--display")
+	.addIndicators()				
+	.addTo(controller);
+	
+// Setting the Fade In 
+
+var fadeInDescriptionIut = new ScrollMagic.Scene({
+		triggerElement: ".story__finland",
+		duration:25*vh(),
+		offset:50*vh()
+	})		
+				
+	.setTween(".story__wrapper-description-finland", 0.2, {opacity: "1" })	
+	.addIndicators()				
+	.addTo(controller);
+
+
+// Setting the Fade Out 
+	
+var fadeOutDescriptionIut = new ScrollMagic.Scene({
+	triggerElement: ".story__finland",
+	duration:25*vh(),
+	offset:150*vh()
+	})						
+	
+	.setTween(".story__wrapper-description-finland", 0.2, {opacity: "0" })	
+	.addIndicators()				
+	.addTo(controller); 
+
+
+
+
+
+
+
+//-------------LINE BACK TO FRANCE ------------//
+	
+	
+// prepare SVG
+
+var $francePath = $("#story__line-to-france");
+pathPrepare($francePath);
+
+
+// Setting of the tween 
+
+var drawCanadaFranceLine = new TimelineMax()
+	.add(TweenMax.to($francePath, 0.9, {strokeDashoffset: 0, ease:Linear.easeNone})) // draw word for 0.9
+
+
+// Display the div that contains the line 
+
+var displayLineToFrance = new ScrollMagic.Scene({
+	triggerElement: ".story__finland",
+	duration: 100*vh(),
+	offset:150*vh(),
+	})
+	
+	.setClassToggle(".story__wrapper-line-to-france","story__wrapper-line-to-france--display")
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
+	
+	
+// Draw the line  
+		
+var drawLineToFrance = new ScrollMagic.Scene({
+	triggerElement: ".story__finland",
+	duration: 50*vh(),
+	offset: 150*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween(drawCanadaFranceLine)
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller);
+	
+	
+// Erase the line 
+
+var eraseLineToFrance = new ScrollMagic.Scene({
+	triggerElement: ".story__finland",
+	duration: 50*vh(),
+	offset: 200*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween("#story__line-to-france", 0.2, {opacity: "0" })
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller); 
+	
+var upLineToFinland = new ScrollMagic.Scene({
+	triggerElement: ".story__finland",
+	duration: 50*vh(),
+	offset: 200*vh(),
+	tweenChanges: true
+	})
+	
+	.setTween(".story__wrapper-line-to-france", 0.2, {top: "-200px"})
+	.addIndicators() // add indicators (requires plugin)
+	.addTo(controller); 
+	
+
+	
 });//End of .ready(function())
+
+
+
+
+
+
+
+
+
+
 
 
 function pathPrepare ($el) {
@@ -229,7 +601,7 @@ function pathPrepare ($el) {
 		var lineLength = $el[0].getTotalLength();
 		$el.css("stroke-dasharray", lineLength);
 		$el.css("stroke-dashoffset", lineLength);
-	}
+}
 
 function whatRowIsOpenTablet(array)//realization section --- Tablet, return the number of row open
 {
@@ -389,5 +761,5 @@ function screenHeight() //return the height of the screen
 	return $(window).height();
 } 
 
-var htmlAll = document.getElementsByTagName('html')[0]; // Select the html object 
+var htmlAll = document.getElementsByTagName('html')[0]; // Select the 'html' object 
 var rem = parseFloat(window.getComputedStyle(htmlAll, null).getPropertyValue("font-size"),10); // Get the font-size and remove the 'px'
